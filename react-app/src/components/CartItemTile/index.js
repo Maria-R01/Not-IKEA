5cc636f56b9a82db48a7958aa6d6b456818ab437
Maria <femaleintech23@gmail.com>
2023-10-07 17:19:57 -0700

    progress

--- react-app/src/components/CartItemTile/index.js
+++ react-app/src/components/CartItemTile/index.js
@@ -1,48 +1,41 @@
 import './CartItemTile.css';
 
 const CartItemTile = ({cartItem, items }) => {
+    // console.log('ITEMS: ',items)
+    const itemIndex = items.findIndex(item => cartItem.item_id === item.id)
+    const itemForCartItem = items[itemIndex]
+    // const imageUrl = itemForCartItem?.images[0]?.url;
+    
 
     return (
         <>
         <div className='item-in-cart'>
         <div className='item-image'>
-            <img></img>
+            <img src={itemForCartItem?.images[0].url}></img>
         </div>
         <div className='item-details-container'>
         <div className='top-half'>
             <div className='item-details'>
-                    <div></div>
+                    <div>{itemForCartItem?.item_name}</div>
             </div>
             <div className='item-price'>
-                    <div></div>
+                    <div>$ {itemForCartItem?.price}</div>
             </div>
         </div>
         <div className='bottom-half'>
             <div className='update-delete-cart-item-container'>
                 <div className='quantity'>
-                    <div></div>
+                    <div className='quantity-buttons-container'>
+                        <button>-</button>
+                        <div>Quantity in cart: {cartItem.quantity}</div>
+                        <button>+</button>
+                    </div>
                 </div>
                 <div className='removal'>
-                    <div></div>
+                    <button>Remove From Cart</button>
                 </div>
             </div>
         </div>
-        </div>
-    </div>
-    <div className='order-summary-container'>
-        <div className='order-summary'>
-            <div>
-                Order Summary
-            </div>
-            <div className='subtotal'>
-                Total (Excluding Taxes)
-                <div>
-                    $
-                </div>
-            </div>
-        </div>
-        <div className='checkout-button-container'>
-            <button>Continue To Checkout</button>
         </div>
     </div>
     </>
