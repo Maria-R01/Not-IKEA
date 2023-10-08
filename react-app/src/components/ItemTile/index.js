d97e46c70100f7ea6030d79b1545aee62665fcad
Maria <femaleintech23@gmail.com>
2023-10-08 14:14:19 -0700

    item tile link

--- react-app/src/components/ItemTile/index.js
+++ react-app/src/components/ItemTile/index.js
@@ -1,19 +1,23 @@
+import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 import './ItemTile.css';
 
 const ItemTile = ({ item }) => {
+    console.log('item in itemTile: ', item) //need to add avg rating and rating count
     return (
         <>
-            <div key={item.id}>
-                        <div>
-                            Name: {item.item_name}
-                        </div>
-                        <div className='itemTile-image-container'>
-                            <img src={item.images[0].url}></img>
-                        </div>
-                        <div>
-                            Price: ${item.price}
-                        </div>
-            </div>
+            <NavLink to={`/item/${item.id}`}>
+                <div key={item.id}>
+                            <div>
+                                Name: {item.item_name}
+                            </div>
+                            <div className='itemTile-image-container'>
+                                <img src={item.images[0].url}></img>
+                            </div>
+                            <div>
+                                Price: ${item.price}
+                            </div>
+                </div>
+            </NavLink>
         </>
     )
 
